7b2b7a90d0a4cc8aff4a3c8b4fb6d806d5e0e6ce
niubin <niubin@mail.ustc.edu.cn>
2017-03-16 18:48:42 +0800

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,54 +1,43 @@
 /*时间有限，写个最简单的图*/
 #include<stdio.h>
-#define N 10
-char Vertex[10] = { 'a','b','c','d','e','f','g','h','g','k' };
+
+#define N 3
+char Vertex[N] = { 'a','b','c'};
 bool AdjMatrix[N][N] = { false };
-bool Visited[10] = { false };
-void CreateGraph(bool AgiMatrix[][10])
-{
-	bool no = false;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			scanf("%d", &no);
-			AgiMatrix[i][j] = no;
-		}
-	}
+bool Visited[N] = { false };
+void CreateGraph(bool AgiMatrix[][N]) {
+    bool no = false;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &no);
+            AgiMatrix[i][j] = no;
+        }
+    }
 
 }
-void PrintGraph(bool AdjMatrix[][10], char Vertex[10])
-{
-	for (int i = 0; i < N; i++)
-	{
-		printf("%c", Vertex[i]);
-		printf("	");
-		for (int j = 0; j < N; j++)
-		{
-			printf("%d\t", AdjMatrix[i][j]);
-		}
-	}
+void PrintGraph(bool AdjMatrix[][N], char Vertex[N]) {
+    for (int i = 0; i < N; i++) {
+        printf("%c", Vertex[i]);
+        printf("	");
+        for (int j = 0; j < N; j++) {
+            printf("%d\t", AdjMatrix[i][j]);
+        }
+    }
 }
-void TraverseGraph(bool AdjMatrix[][10], char Vertex[10], int bgn)
-{
-	printf("%c", Vertex[bgn]);
-	for (int i = 0; i < N; i++)
-	{
-		if (AdjMatrix[bgn][i] != 0 && Visited[i] == false)
-		{
-			
-			bgn = i;
-			TraverseGraph(AdjMatrix, Vertex, bgn);
-		}
-			
-	}
-	return;
-}
-int main(int argc, char* argv[])
-{
-	CreateGraph(AdjMatrix);
-	TraverseGraph(AdjMatrix, Vertex, 0);
-	
-}
+void TraverseGraph(bool AdjMatrix[][N], char Vertex[N], int bgn) {
+    printf("%c", Vertex[bgn]);
+    Visited[bgn]=true;
+    for (int i = 0; i < N; i++) {
+        if (AdjMatrix[bgn][i] != 0 && Visited[i] == false) {
+            bgn = i;
+            TraverseGraph(AdjMatrix, Vertex, bgn);
+        }
 
-	
+    }
+    return;
+}
+int main(void) {
+    CreateGraph(AdjMatrix);
+    PrintGraph(AdjMatrix, Vertex);
+
+}

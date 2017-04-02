5258081f83ca6658f5d03aee7a5498e75c73951f
niubin <niubin@mail.ustc.edu.cn>
2017-04-02 23:19:14 +0800

    Update stack.h

--- maze/stack.h
+++ maze/stack.h
@@ -1,1 +1,16 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "base.h"
+
+typedef struct NodeType {
+	ElemType data;
+	NodeType *next;
+
+}NodeType,*LinkType;//节点指针，指针类型
+typedef struct
+{
+	LinkType top;
+	int size;
+}Stack;//栈的类型
+

c76d7c43249acc8c4ddb5935af5af4ba4b3b9701
niubin <niubin@mail.ustc.edu.cn>
2017-06-19 13:51:48 +0800

    Create log.h

---
+++ marco/log.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2017/6/19.
+//
+
+#ifndef MARCO_LOG_H
+#define MARCO_LOG_H
+#define LOG(format, ...)     fprintf(file, format, ##__VA_ARGS__)
+#endif //MARCO_LOG_H

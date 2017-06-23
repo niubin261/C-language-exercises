5eea32ea557c8979bb40c0eb79d8b289cb925dd3
niubin <niubin@mail.ustc.edu.cn>
2017-06-23 19:30:32 +0800

    Update strtok.c
    how to use strtok to split string

--- strtok.c
+++ strtok.c
@@ -7,7 +7,7 @@ int main ()
     char str[] ="192.168.109.192";
     char * pch;
     printf ("Splitting string \"%s\" into tokens:\n",str);
-    pch = strtok (str," .");
+    pch = strtok (str," .");////-->192\0168.109.192
     while (pch != NULL)
     {
         int pchnum=atoi(pch);
